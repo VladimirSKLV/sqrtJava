42291d9ceac55cc17b80c0fbeefdddf226317116
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-08-15 14:10:55 +0300

    v2

--- src/main/java/ru/netology/javaqamvn/services/SQRService.java
+++ src/main/java/ru/netology/javaqamvn/services/SQRService.java
@@ -1,12 +1,13 @@
+
 package ru.netology.javaqamvn.services;
 
 public class SQRService {
 
-    public int calcSqrt(int from, int to) {
+    public int calcSqrt(int unRange, int upRange) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
             int sqrt = i*i;
-            if (sqrt >= from && sqrt <= to) {
+            if (sqrt >= unRange && sqrt <= upRange) {
                 count++;
             }
         }
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,22 +1,27 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqamvn.services.SQRService;
 
-public class SQRServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Test
-    public void calcSqr() {
-        SQRService service = new SQRService();
-        int expected = 6;
-        int actual = service.calcSqrt(10, 99);
-        Assertions.assertEquals(expected, actual);
-    }
+class SQRServiceTest {
 
-    @Test
-    public void calcSqrFal() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Min amount, 1, 3, 0",
+            "Max amount, 100, 9999, 90",
+            "'Min interval where amount 1', 144, 144, 1",
+            "'Min interval where amount 0', 145, 145, 0",
+            "Interval under 99, 1, 99, 0",
+            "Interval upper 9802, 9802, 99999, 0",
+    })
+    void shouldCalculate(String testName, int unRange, int upRange, int expected) {
         SQRService service = new SQRService();
-        int expected = 10;
-        int actual = service.calcSqrt(10, 99);
-        Assertions.assertEquals(expected, actual);
+
+        int actual = service.calcSqrt(unRange, upRange);
+
+        Assertions.assertEquals(actual, expected);
+        System.out.println(actual);
     }
 }
